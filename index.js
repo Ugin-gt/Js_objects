fafe196da7376f68f2bd378bf880c2b0e3f791f7
Ugin-gt <apach575@gmail.com>
2021-01-12 16:29:24 +0200

    Add JS objects practice

---
+++ index.js
@@ -0,0 +1,97 @@
+"use strict";
+
+// function countVowels(str) {
+
+//   const arrayVowels = ["a", "e", "i", "o", "u"];
+
+//   return str
+//   .split(' ');
+//   .filter((char)) => arrayVowels.includes
+
+class UserClass {
+  constructor(name, surname, age) {
+    this.name = name;
+    this.surname = surname;
+    this.age = age;
+  }
+  getFullName() {
+    return `${this.name} ${this.surname}`;
+  }
+
+  isAdult() {
+    return this.age >= 18;
+  }
+}
+const user = new UserClass("Vlad", "Test", 50);
+const user2 = new UserClass("Masha", "Medved", 10);
+
+class WorkerSalary {
+  constructor(nameW, surnameW, workingdays, salaryforday) {
+    this.nameW = nameW;
+    this.surnameW = surnameW;
+    this.workingdays = workingdays;
+    this.salaryforday = salaryforday;
+  }
+  getSalary() {
+    return this.salaryforday * this.workingdays;
+  }
+}
+const AdminSalary = new WorkerSalary("Masha", "Medvedeva", 10, 500);
+
+// Вес авто с топливом
+
+class Auto {
+  constructor(model, weight) {
+    this.model = model;
+    this.ownWeight = weight;
+  }
+}
+
+class Fuel {
+  constructor(fuel, volume, density) {
+    this.fuel = fuel;
+    this.volume = volume;
+    this.density = density;
+  }
+  getWeight() {
+    return this.volume * this.density;
+  }
+}
+return TotalAutoWeight =(this.weight)+ Fuel(getWeight(this.volume*this.density));
+
+//const TotalAutoWeight = new AutoWeight(BMW, 1580, 0.81);
+
+
+class Friend {
+  constructor (name,appleAmount,friend) {
+this.name = name;
+this.appleAmount = appleAmount;
+this.friend = friend;
+  }
+
+getSumApple() {
+
+  return this.appleAmount + this.friend.appleAmount ;
+  } 
+};
+
+
+
+
+
+
+constructor(volume1, density1) {
+  this.volume1 = volume1;
+  this.density1 = density1;
+    }
+
+    set volume1 (v) {
+      if (typeof v !== 'number') {
+        throw new TypeError ('Data must be a numbers');
+
+      }
+    }
+  get volume1 (v) {
+    // getter
+    return this._volume;
+      }
