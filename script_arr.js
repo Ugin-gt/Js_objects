7a7ef8f20b617915588c6f997a9b01cd2353c18c
Ugin-gt <apach575@gmail.com>
2021-01-15 00:01:49 +0200

    Add script.js practice with Symbol

---
+++ script_arr.js
@@ -0,0 +1,24 @@
+const mySymbol = Symbol('Просто метка');
+const mySymbol2 = Symbol(0);
+const mySymbol3 = Symbol('Просто метка');
+
+
+
+console.log(mySymbol === mySymbol3);
+
+const users = [
+  {
+    name: 'name surname'
+  },
+  {
+    name: 'name surname2'
+  }
+];
+
+function sum(...args) {
+  let result = 0;
+  for (const number of args) {
+    result += a
+  }
+  return result;
+};
